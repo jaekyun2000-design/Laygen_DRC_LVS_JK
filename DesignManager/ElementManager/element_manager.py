530757580ce789c1291a2eeb3d43dc0ae296b232
sun9u <62418003+sun9u@users.noreply.github.com>
2020-11-11 17:00:40 +0900

    Element Manager added
    Blueprint of element manager class.

---
+++ DesignManager/ElementManager/element_manager.py
@@ -0,0 +1,35 @@
+
+
+class ElementManager:
+    def __init__(self):
+        self.elementParameterDict = dict()
+        self.elementConstraintDict = dict()
+
+    def load_element_from_designParameter(self,designParameter):
+        ## self.elementDict['dp name] = designPa
+
+        ## check const same,,
+        ## no : desicng constarin make..
+        ## yes : data update
+
+        pass
+
+    def load_element_from_designConstratin(self,designConstraint):
+        ## self.constDict['dp name] = designPa
+
+        pass
+
+    def update_designParameter(self):
+
+
+
+        return depa
+        pass
+
+    def update_designConstraint(self):
+        pass
+        return dc
+
+
+if __name__ == '__main__':
+    dp1 = None
