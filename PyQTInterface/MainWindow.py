c80bc2a1b82c9933a6f74eecb531edaa515c42ff
sun9u <62418003+sun9u@users.noreply.github.com>
2020-11-16 17:15:06 +0900

    Element Manager compatible
    'create new design parameter' and 'create new design constraint AST' functions are revised to support updated qt_interface functions.

--- PyQTInterface/MainWindow.py
+++ PyQTInterface/MainWindow.py
@@ -714,26 +714,30 @@ class _MainWindow(QMainWindow):
             self.warning.show()
             self.dockContentWidget4ForLoggingMessage._WarningMessage("Create DesignParameter Fail: There is no Module")
         else:
-            _designParameterID = self._QTObj._qtProject._getDesignParameterId(self._CurrentModuleName)
-            _newDesignID = (self._CurrentModuleName + str(_designParameterID))
-            self._QTObj._qtProject._createNewDesignParameter(_id = _newDesignID, _type= _DesignParameter['_DesignParametertype'], _ParentName= self._CurrentModuleName)
-            print(self._QTObj._qtProject._DesignParameter[self._CurrentModuleName][_newDesignID]._DesignParameter)
-            for key in _DesignParameter:
-                self._QTObj._qtProject._DesignParameter[self._CurrentModuleName][_newDesignID]._setDesignParameterValue(_index = key, _value= _DesignParameter[key])
-            self._QTObj._qtProject._DesignParameter[self._CurrentModuleName][_newDesignID]._setDesignParameterName(_DesignParameterName=_DesignParameter['_DesignParameterName'])
-
-            print(self._QTObj._qtProject._DesignParameter[self._CurrentModuleName][_newDesignID])
-
-            print(self._QTObj._qtProject._DesignParameter[self._CurrentModuleName][_newDesignID]._DesignParameter)
-            # self._QTObj._qtProject._DesignParameter[self._CurrentModuleName][_newDesignID]._updateVisualItem()
-            # visualItem = self._QTObj._qtProject._DesignParameter[self._CurrentModuleName][_newDesignID]._VisualizationItemObj
-            if self._QTObj._qtProject._DesignParameter[self._CurrentModuleName][_newDesignID]._type == 2:
-                self._QTObj._qtProject._DesignParameter[self._CurrentModuleName][_newDesignID]._XYCoordinatesForDisplay = _DesignParameter['_XYCoordinates']
-
-
-            visualItem = self.createVisualItemfromDesignParameter(self._QTObj._qtProject._DesignParameter[self._CurrentModuleName][_newDesignID])
+            design_dict = self._QTObj._qtProject._feed_design(design_type='parameter', module_name= self._CurrentModuleName, dp_dict= _DesignParameter)
+            visualItem = self.createVisualItemfromDesignParameter(
+                self._QTObj._qtProject._DesignParameter[self._CurrentModuleName][design_dict['parameter_id']])
             self.updateGraphicItem(visualItem)
             self.dockContentWidget4ForLoggingMessage._InfoMessage("Design Parameter Created")
+            #
+            #
+            # _designParameterID = self._QTObj._qtProject._getDesignParameterId(self._CurrentModuleName)
+            # _newDesignID = (self._CurrentModuleName + str(_designParameterID))
+            # self._QTObj._qtProject._createNewDesignParameter(_id = _newDesignID, _type= _DesignParameter['_DesignParametertype'], _ParentName= self._CurrentModuleName)
+            # print(self._QTObj._qtProject._DesignParameter[self._CurrentModuleName][_newDesignID]._DesignParameter)
+            # for key in _DesignParameter:
+            #     self._QTObj._qtProject._DesignParameter[self._CurrentModuleName][_newDesignID]._setDesignParameterValue(_index = key, _value= _DesignParameter[key])
+            # self._QTObj._qtProject._DesignParameter[self._CurrentModuleName][_newDesignID]._setDesignParameterName(_DesignParameterName=_DesignParameter['_DesignParameterName'])
+            #
+            # print(self._QTObj._qtProject._DesignParameter[self._CurrentModuleName][_newDesignID])
+            #
+            # print(self._QTObj._qtProject._DesignParameter[self._CurrentModuleName][_newDesignID]._DesignParameter)
+            # # self._QTObj._qtProject._DesignParameter[self._CurrentModuleName][_newDesignID]._updateVisualItem()
+            # # visualItem = self._QTObj._qtProject._DesignParameter[self._CurrentModuleName][_newDesignID]._VisualizationItemObj
+            # if self._QTObj._qtProject._DesignParameter[self._CurrentModuleName][_newDesignID]._type == 2:
+            #     self._QTObj._qtProject._DesignParameter[self._CurrentModuleName][_newDesignID]._XYCoordinatesForDisplay = _DesignParameter['_XYCoordinates']
+
+
 
 
     def deleteDesignParameter(self,_ID):
@@ -900,11 +904,14 @@ class _MainWindow(QMainWindow):
             self.warning.show()
             self.dockContentWidget4ForLoggingMessage._WarningMessage("Create DesignConstraint Fail: There is no Module")
         else:
-            a, ids = self._QTObj._qtProject._createNewDesignConstraintAST(_ASTDtype='ASTsingle', _AST=_AST,_ParentName=self._CurrentModuleName)
-            id = ids[0]
-            self.dockContentWidget3_2.createNewConstraintAST(_id = id, _parentName= self._CurrentModuleName, _DesignConstraint = self._QTObj._qtProject._DesignConstraint)
-            pass
-            # STMTList , ids = self._QTObj._qtProject._createNewDesignConstraintAST( _ASTDtype = 'STMT', _STMTList = _STMT ,_ParentName=self._CurrentModuleName)
+            design_dict = self._QTObj._qtProject._feed_design(design_type='constraint', module_name=self._CurrentModuleName, _ast= _AST)
+            self.dockContentWidget3_2.createNewConstraintAST(_id=design_dict['constraint_id'], _parentName=self._CurrentModuleName,
+                                                             _DesignConstraint=self._QTObj._qtProject._DesignConstraint)
+            # a, ids = self._QTObj._qtProject._createNewDesignConstraintAST(_ASTDtype='ASTsingle', _AST=_AST,_ParentName=self._CurrentModuleName)
+            # id = ids[0]
+            # self.dockContentWidget3_2.createNewConstraintAST(_id = id, _parentName= self._CurrentModuleName, _DesignConstraint = self._QTObj._qtProject._DesignConstraint)
+            # pass
+            # # STMTList , ids = self._QTObj._qtProject._createNewDesignConstraintAST( _ASTDtype = 'STMT', _STMTList = _STMT ,_ParentName=self._CurrentModuleName)
             # #_idToSTMTdict = ASTmodule._searchSTMTlistWithIDList(STMTList, ids)
             # _idToSTMTdict = ASTmodule._returnChildIDandAST(STMTList)
             # self.dockContentWidget3_2.createNewConstraintSTMTList(STMTList,_idToSTMTdict)
