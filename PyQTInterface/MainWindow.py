709453dae8673e79080fbb37c400014c61b0091d
chlee9446 <chlee9446@postech.ac.kr>
2021-06-01 15:56:51 +0900

    XY 수정

--- PyQTInterface/MainWindow.py
+++ PyQTInterface/MainWindow.py
@@ -1234,7 +1234,7 @@ class _MainWindow(QMainWindow):
         self._QTObj._qtProject._DesignParameter[module][dp_id]._DesignParameter['_ModelStructure'] = _dp['_ModelStructure']
         self._QTObj._qtProject._DesignParameter[module][dp_id]._DesignParameter['library'] = tmp_dp_dict['library']
         self._QTObj._qtProject._DesignParameter[module][dp_id]._DesignParameter['className'] = tmp_dp_dict['className']
-        self._QTObj._qtProject._DesignParameter[module][dp_id]._DesignParameter['XY'] = tmp_dp_dict['XY']
+        self._QTObj._qtProject._DesignParameter[module][dp_id]._DesignParameter['XY'] = tmp_dp_dict['_XYCoordinates']
         self._QTObj._qtProject._DesignParameter[module][dp_id]._DesignParameter['calculate_fcn'] = tmp_dp_dict['calculate_fcn']
         self._QTObj._qtProject._DesignParameter[module][dp_id]._DesignParameter['parameters'] = tmp_dp_dict['parameters']
 
