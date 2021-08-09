1904ea161fcbaeac542698d865e2d1e43500cea0
sun9u <sgjeong@postech.ac.kr>
2021-08-09 15:32:43 +0900

    interface_delegator.py -> 임시

--- PyQTInterface/delegator/interface_delegator.py
+++ PyQTInterface/delegator/interface_delegator.py
@@ -1,1 +1,21 @@
-pass
+from PyQTInterface import SetupWindow
+
+class Delegator:
+    def __init__(self, main_window):
+        self.main_window= main_window
+
+    def create_widget(self, widget_name, widget_type):
+        pass
+
+    def close_widget(self, widget_name):
+        pass
+    
+    
+    
+class SetupWindowDelegator(Delegator):
+    def __init__(self):
+        super(SetupWindowDelegator, self).__init__()
+
+    def make_boundary_window(self):
+        self.main_window.bw = SetupWindow._BoundarySetupWindow()
+        self.main_window.bw.show()
