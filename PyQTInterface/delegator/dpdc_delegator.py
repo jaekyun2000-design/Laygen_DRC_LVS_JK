d9d8a087c086d46a04f42bad12fc83be189e4d31
sun9u <sgjeong@postech.ac.kr>
2021-09-03 15:52:09 +0900

    dp update code 추가

--- PyQTInterface/delegator/dpdc_delegator.py
+++ PyQTInterface/delegator/dpdc_delegator.py
@@ -16,7 +16,7 @@ class DesignDelegator(delegator.Delegator):
         design_dict['parameter'].update_unified_expression()
         self.create_vs_item(design_dict['parameter'])
         if design_dict['constraint']:
-            self.add_constraint_to_treeview(design_dict['constraint_id'])
+            self.control_constraint_tree_view(design_dict['constraint_id'])
 
     def create_qt_constraint(self, constraint_ast=None, constraint_dict=None):
         """
@@ -29,7 +29,7 @@ class DesignDelegator(delegator.Delegator):
             design_dict = self.main_window._QTObj._qtProject._feed_design(design_type='constraint',
                                                                           odule_name= self.main_window._CurrentModuleName,
                                                                           _ast=constraint_ast)
-            self.add_constraint_to_treeview(design_dict['constraint_id'])
+            self.control_constraint_tree_view(design_dict['constraint_id'])
             if design_dict['parameter']:
                 self.create_vs_item(design_dict['parameter'])
         elif constraint_dict:
@@ -58,13 +58,67 @@ class DesignDelegator(delegator.Delegator):
             self.main_window.scene.getNonselectableLayerList)
         self.main_window.dockContentWidget1_2.layer_table_widget.updateLayerList(self.main_window._layerItem)
 
-    def add_constraint_to_treeview(self, constraint_id):
+    def control_constraint_tree_view(self, constraint_id, channel=2, request='post' ):
         """
-        receive: constraint_id
-        process: post constraint info on tree widget
+        receive: constraint_id, channel (generator(1) or candidate(2), both(3)), request (post or delete or update)
+        process: control constraint info on tree widget
         request: nothing
         return : nothing
         """
-        self.main_window.dockContentWidget3_2.createNewConstraintAST(_id=constraint_id,
-                                                                     _parentName=self.main_window._CurrentModuleName,
-                                                                     _DesignConstraint=self.main_window._QTObj._qtProject._DesignConstraint)
+        if channel == 1:
+            targets = [self.main_window.dockContentWidget3]
+        elif channel == 2:
+            targets = [self.main_window.dockContentWidget3_2]
+        else:
+            targets = [self.main_window.dockContentWidget3, self.main_window.dockContentWidget3_2]
+
+        for target in targets:
+            if request == 'post':
+                target.createNewConstraintAST(_id=constraint_id, _parentName=self.main_window._CurrentModuleName,
+                                              _DesignConstraint=self.main_window._QTObj._qtProject._DesignConstraint)
+            elif request == 'update':
+                target.update_constraint_by_id(constraint_id)
+
+
+    def update_qt_parameter(self, dp_dict, element_manager_update=True):
+        """
+        receive: design parameter dictionary
+        process: update qt_dp by dp_dict
+        request: update vs item
+        return : nothing
+        """
+        legacy_id = dp_dict['_id']
+        module = self.main_window._CurrentModuleName
+        
+        if dp_dict['_id'] != dp_dict['_ElementName']:
+            self.main_window.visualItemDict[dp_dict['_ElementName']] = self.main_window.visualItemDict.pop(dp_dict['_id'])
+
+
+        for key in dp_dict:
+            self.main_window._QTObj._qtProject.dp_dict[module][legacy_id]._setDesignParameterValue(_index = key, _value= dp_dict[key])
+        self.main_window._QTObj._qtProject.dp_dict[module][legacy_id]._setDesignParameterName(dp_dict['_ElementName'])
+        design_dict = self.main_window._QTObj._qtProject._update_design(design_type='parameter', module_name=self.main_window._CurrentModuleName,
+                                                          dp_dict=dp_dict, id=legacy_id, element_manager_update =element_manager_update,)
+        design_dict['parameter'].update_unified_expression()
+        self.update_vs_item(design_dict['parameter'])
+
+        if design_dict['constraint_id']:
+            self.control_constraint_tree_view(design_dict['constraint_id'],channel=3,request='update')
+
+    def update_qt_constraint(self):
+        pass
+
+    def update_vs_item(self, qt_design_parameter):
+        if qt_design_parameter is None:
+            return None
+
+        id = qt_design_parameter._id
+
+        if id in self.main_window.visualItemDict:
+            self.main_window.visualItemDict[id].updateTraits(qt_design_parameter._DesignParameter)
+            self.main_window.visualItemDict[id]._id = id
+            self.main_window.visualItemDict[id]._ItemTraits['_id'] = id
+            self.main_window.visualItemDict[id]._ItemTraits['_ElementName'] = id
+            self.main_window.updateGraphicItem(self.main_window.visualItemDict[id])
+            return self.main_window.visualItemDict[id]
+        return None
