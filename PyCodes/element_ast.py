62fcca5e7f3e84f37206e43953721b97ab4d206c
Minsu Kim <kms95@postech.ac.kr>
2021-09-06 19:09:15 +0900

    transformer boundary: Logic Expression, XYCoordinates 지원

--- PyCodes/element_ast.py
+++ PyCodes/element_ast.py
@@ -3,6 +3,7 @@ import copy
 import warnings
 
 import astunparse
+from PyCodes import variable_ast
 # listTypeData = ['Lib','tb','PlaceDef','RouteDef','DRCDef','Iteration','P_R']
 custom_ast_list = ['Generator','Sref','Boundary','Path', 'Text', 'MacroCell']
 #--start constants--
@@ -225,9 +226,29 @@ class ElementTransformer(ast.NodeTransformer):
             if field == 'XY':
                 continue
             if isinstance(node.__dict__[field], ast.AST):
-                node.__dict__[field] = astunparse.unparse(node.__dict__[field]).replace('\n', '')
+                if type(node.__dict__[field]) == 'LogicExpression':
+                    tmp_node = variable_ast.LogicExpression()
+                    tmp_node.id = node.__dict__[field].id
+                    tmp_node.info_dict = node.__dict__[field].info_dict
+                    tmp_code_ast = variable_ast.IrregularTransformer().visit_LogicExpression(tmp_node)
+                elif type(node.__dict__[field]) == 'XYCoordinate':
+                    tmp_node = variable_ast.XYCoordinate()
+                    tmp_node.id = node.__dict__[field].id
+                    tmp_node.info_dict = node.__dict__[field].info_dict
+                    tmp_code_ast = variable_ast.IrregularTransformer().visit_XYCoordinate(tmp_node)
             elif type(node.__dict__[field]) == list and isinstance(node.__dict__[field][0], ast.AST):
-                node.__dict__[field] = astunparse.unparse(node.__dict__[field]).replace('\n', '')
+                if type(node.__dict__[field][0]) == 'LogicExpression':
+                    tmp_node = variable_ast.LogicExpression()
+                    tmp_node.id = node.__dict__[field][0].id
+                    tmp_node.info_dict = node.__dict__[field][0].info_dict
+                    tmp_code_ast = variable_ast.IrregularTransformer().visit_LogicExpression(tmp_node)
+                elif type(node.__dict__[field][0]) == 'XYCoordinate':
+                    tmp_node = variable_ast.XYCoordinate()
+                    tmp_node.id = node.__dict__[field][0].id
+                    tmp_node.info_dict = node.__dict__[field][0].info_dict
+                    tmp_code_ast = variable_ast.IrregularTransformer().visit_XYCoordinate(tmp_node)
+                tmp_code = astunparse.unparse(tmp_code_ast)
+                node.__dict__[field] = tmp_code
 
         if syntax == 'list' :#or syntax == 'string':
             tmp_xy = str(node.XY).replace("'","")
@@ -364,204 +385,18 @@ class ElementTransformer(ast.NodeTransformer):
         tmp = ast.parse(sentence)
         return tmp.body[0]
 
+def run_transformer(source_ast):
+    module_ast = ast.Module()
+    if type(source_ast) == list:
+        module_ast.body = copy.deepcopy(source_ast)
+    else:
+        module_ast.body = copy.deepcopy([source_ast])
 
-# class MacroTransformer1(ast.NodeTransformer):
-#     def visit_MacroKeyword(self,node):
-#         return ast.copy_location(
-#             ast.keyword(
-#                     arg=node.arg,
-#                     value = MacroListSubscript(
-#                         list_id = node.id,
-#                         list_id_attr = node.attr,
-#                         index1 = node.index1,
-#                         index2 = node.index2
-#                     )
-#                     # value=ast.Subscript(
-#                     #     value=ast.Subscript(
-#                     #         value=ast.Attribute(
-#                     #             value=ast.Name(
-#                     #                 id=node.id
-#                     #             ),
-#                     #             attr=node.attr
-#                     #         ),
-#                     #         slice=node.index1
-#                     #     ),
-#                     #     slice=node.index2
-#                     # )
-#                 )
-#             ,node
-#         )
-#
-# class MacroTransformer2(ast.NodeTransformer):
-#     def visit_MacroListSubscript(self,node):
-#         return ast.copy_location(
-#             ast.Subscript(
-#                 value=ast.Subscript(
-#                     value=ast.Attribute(
-#                         value=ast.Name(
-#                             id=node.list_id
-#                         ),
-#                         attr=node.list_id_attr
-#                     ),
-#                     slice=node.index1
-#                 ),
-#                 slice=node.index2
-#             ),
-#             node
-#         )
-#
-#
-# class MacroSubscript(ast.AST):
-#     _fields = (
-#         'id',
-#         'attr',
-#         'index',
-#     )
-#
-# class MacorSubscriptTransformer(ast.NodeTransformer):
-#     def visit_MacroSubscript(self,node):
-#         for field in node._fields:
-#             if not field in node.__dict__:
-#                 node.__dict__[field] = None
-#         return ast.copy_location(
-#             ast.Subscript(
-#                 value=ast.Attribute(
-#                     value=ast.Name(
-#                         id=node.id
-#                     ),
-#                     attr=node.attr
-#                 ),
-#                 slice=node.index
-#             ),
-#             node
-#         )
-#
+    result_ast = variable_ast.IrregularTransformer().visit(module_ast)
+    result_ast = variable_ast.VariableTransformer().visit(result_ast)
+    result_ast = ElementTransformer().visit(result_ast)
 
-    ###### Below is the partial genuine ast format of Boundary Element : Do not delete #######
-
-    #     return ast.Assign(
-    #             targets=[ast.Name(
-    #                 id=node.name
-    #             )],
-    #             value=ast.Call(
-    #                 func=ast.Attribute(
-    #                     value=ast.Name(
-    #                         id='self'
-    #                     ),
-    #                     attr='_BoundaryElementDeclaration'
-    #                 ),
-    #                 args=[]
-    #                 ,
-    #                 keywords=[
-    #                     MacroKeyword(
-    #                         arg='_Layer',
-    #                         id="DesignParameters",
-    #                         attr='LayerMapping',
-    #                         index1=ast.Str(
-    #                             s=node.layer
-    #                         ),
-    #                         index2=ast.Num(
-    #                             n=0
-    #                         )
-    #                     ),
-    #                     MacroKeyword(
-    #                         arg='_Datatype',
-    #                         id="DesignParameters",
-    #                         attr='LayerMapping',
-    #                         index1=ast.Str(
-    #                             s=node.layer
-    #                         ),
-    #                         index2=ast.Num(
-    #                             n=1
-    #                         ),
-    #                     )
-    #                 ]
-    #             )
-    #         )
-
-
-
-
-        # return ast.Assign(
-        #         targets=[ast.Name(
-        #             id=node.name
-        #         )],
-        #         value=ast.Call(
-        #             func=ast.Attribute(
-        #                 value=ast.Name(
-        #                     id='self'
-        #                 ),
-        #                 attr='_PathElementDeclaration'
-        #             ),
-        #             args=[]
-        #             ,
-        #             keywords=[
-        #                 MacroKeyword(
-        #                     arg = '_Layer',
-        #                     id = "DesignParameters",
-        #                     attr='LayerMapping',
-        #                     index1 = ast.Str(
-        #                         s = node.layer
-        #                     ),
-        #                     index2 = ast.Num(
-        #                         n = 0
-        #                     )
-        #                 ),
-        #                 MacroKeyword(
-        #                     arg='_Datatype',
-        #                     id="DesignParameters",
-        #                     attr='LayerMapping',
-        #                     index1=ast.Str(
-        #                         s=node.layer
-        #                     ),
-        #                     index2=ast.Num(
-        #                         n=1
-        #                     ),
-        #                 )
-        #             ]
-        #         )
-        #     )
-
-
-        # return ast.Assign(
-        #         targets=[ast.Name(
-        #             id=node.name
-        #         )],
-        #         value=ast.Call(
-        #             func=ast.Attribute(
-        #                 value=ast.Name(
-        #                     id='self'
-        #                 ),
-        #                 attr='_SrefElementDeclaration'
-        #             ),
-        #             args=[]
-        #             ,
-        #             keywords=[
-        #                 MacroKeyword(
-        #                     arg='_Layer',
-        #                     id="DesignParameters",
-        #                     attr='LayerMapping',
-        #                     index1=ast.Str(
-        #                         s=node.layer
-        #                     ),
-        #                     index2=ast.Num(
-        #                         n=0
-        #                     )
-        #                 ),
-        #                 MacroKeyword(
-        #                     arg='_Datatype',
-        #                     id="DesignParameters",
-        #                     attr='LayerMapping',
-        #                     index1=ast.Str(
-        #                         s=node.layer
-        #                     ),
-        #                     index2=ast.Num(
-        #                         n=1
-        #                     ),
-        #                 )
-        #             ]
-        #         )
-        #     )
+    return result_ast
 
 
 if __name__ == '__main__':
@@ -599,30 +434,3 @@ if __name__ == '__main__':
     pt = ElementTransformer().visit_Path(b)
     # st = ElementTransformer().visit_Sref(c)
     print("DEBUG")
-
-    # ab= MacroSubscript(
-    #     id = 'list1',
-    #     index = ast.Num(
-    #         n=0
-    #     )
-    # )
-    # ab = MacorSubscriptTransformer().visit(ab)
-    # print(astunparse.unparse(ab))
-    #
-    # k = Generator()
-    # k.name = 'FF'
-    # asa = GeneratorTransformer()
-    # kk = asa.visit(k)
-    # astunparse.unparse(kk)
-    # print(astunparse.unparse(kk))
-    # # d = MacroListSubscript(
-    # #     list_id = 'self',
-    # #     list_id_attr = '_DesignParameter',
-    # #     index1 = ast.Str(
-    # #         s = '_ViaMet32Met4OnInLineTop1',
-    # #     ),
-    # #     index2 = ast.Str(
-    # #         s = '_Wdith',
-    # #     )
-    # # )
-    # # dk = MacroTransformer().visit(d)
