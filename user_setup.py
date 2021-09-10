9fa0a4dcd63347dd95940680724c614494efb8f5
sun9u <sgjeong@postech.ac.kr>
2021-09-10 16:38:28 +0900

    user_setup.py에 cell detector 기능을 위한 변수 추가

--- user_setup.py
+++ user_setup.py
@@ -4,5 +4,15 @@ DEBUG = True
 MIN_SNAP_SPACING = 5
 GDS2GEN = True
 
+
+
+
+#########################################################
+# for cell detector model setup #
+DL_FEATURE = True
+
 matrix_x_step = 128
 matrix_y_step = 128
+layer_list = ['DIFF','NIMP','PIMP','POLY','CONT','METAL1', 'METAL2', 'METAL3', 'METAL4', 'METAL5']
+data_type_list = ['C2FF','XOR','NMOS','PMOS','NBODY','PBODY','VIAPOLY1','VIA12', 'VIA23','VIA34','VIA45']
+#########################################################
