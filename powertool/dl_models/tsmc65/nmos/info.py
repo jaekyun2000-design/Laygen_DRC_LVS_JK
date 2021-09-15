2c526a3bbd6545cc5d69b298fb0d4c43013a6d1e
sun9u <sgjeong@postech.ac.kr>
2021-09-15 19:02:42 +0900

    info 수정

--- powertool/dl_models/tsmc65/nmos/info.py
+++ powertool/dl_models/tsmc65/nmos/info.py
@@ -2,11 +2,11 @@ outputs = ['_NMOSChannelWidth', '_NMOSChannellength', '_NMOSNumberofGate', '_NMO
 
 import pickle
 with open('./powertool/dl_models/tsmc65/nmos/ms.pkl', 'rb') as f:
-    data = pickle.load(f)
+    ms_dicts = pickle.load(f)
 # ms_dicts = [dict(mean=0.3340751191994302, std=0.05648022750565867),
 #             dict(mean=0.03390469042325812, std=0.030330695714379165),
 #             dict(mean=10.266, std=5.725231087039195),
 #             None]
-ms_dicts = data.ms_dicts
+# ms_dicts = data
 
 scales = ['height', 'width', None, None]
