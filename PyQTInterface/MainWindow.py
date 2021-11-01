bcdad8930490d721e44b9ae68810015bd9323100
Minsu Kim <kms95@postech.ac.kr>
2021-11-01 13:14:53 +0900

    user setup MIN_SNAP_SPACING 살려두고 Main Rollback.

--- PyQTInterface/MainWindow.py
+++ PyQTInterface/MainWindow.py
@@ -75,7 +75,6 @@ from PyQTInterface import calculator
 from PyQTInterface import ConditionalStatement
 
 from generatorLib import generator_model_api
-from generatorLib import drc_api
 from generatorLib import DRC
 
 ##for easy debug##
@@ -1219,8 +1218,7 @@ class _MainWindow(QMainWindow):
                     del rm_item
 
                 def min_snap_change():
-                    drc_api.drc_classified_dict['MIN_SNAP_SPACING']['_MinSnapSpacing'] = spin_box.value()
-                    # user_setup.MIN_SNAP_SPACING = spin_box.value()
+                    user_setup.MIN_SNAP_SPACING = spin_box.value()
                     self.min_snap_spacing_change.close()
                     print('Process Changed!')
                     self.message = QMessageBox()
@@ -1230,7 +1228,7 @@ class _MainWindow(QMainWindow):
                     self.message.show()
                     self.process_list_widget.close()
 
-                min_snap_before = drc_api.drc_classified_dict['MIN_SNAP_SPACING']['_MinSnapSpacing']
+                min_snap_before = user_setup.MIN_SNAP_SPACING
 
                 self.min_snap_spacing_change = QWidget()
 
@@ -3556,7 +3554,7 @@ class _CustomScene(QGraphicsScene):
         for s_highlighted_rectblock in VisualizationItem._RectBlock.shallow_highlight_list:
             s_highlighted_rectblock.shallow_highlight = False
 
-        snap = drc_api.drc_classified_dict['MIN_SNAP_SPACING']['_MinSnapSpacing']
+        snap = user_setup.MIN_SNAP_SPACING
         x_point = int(int(event.scenePos().toPoint().x() / snap) * snap)
         y_point = int(int(event.scenePos().toPoint().y() / snap) * snap)
         self.send_xy_signal.emit([x_point, y_point])
@@ -3827,7 +3825,7 @@ class _CustomScene(QGraphicsScene):
         #     self.send_move_signal.emit(delta)
         self.oldPos = QGraphicsSceneMouseEvent.scenePos()
 
-        snap = drc_api.drc_classified_dict['MIN_SNAP_SPACING']['_MinSnapSpacing']
+        snap = user_setup.MIN_SNAP_SPACING
         xy = [int(int(QGraphicsSceneMouseEvent.scenePos().x()/snap) * snap), int(int(QGraphicsSceneMouseEvent.scenePos().y()/snap) * snap)]
         self.send_mouse_move_xy_signal.emit(xy)
         self.send_mouse_move_signal.emit(QGraphicsSceneMouseEvent)
