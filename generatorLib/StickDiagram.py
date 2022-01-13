9b54479c96133d80167a1f0913fd9828e529c7cd
IksuJang <isjang@postech.ac.kr>
2022-01-13 13:54:29 +0900

    기타 함수 업데이트

--- generatorLib/StickDiagram.py
+++ generatorLib/StickDiagram.py
@@ -124,7 +124,116 @@ class _StickDiagram:
     #     center2 = self.Center(element_name2)
     #
     #     if
+    def _getSthValue(self, hier_element_tuple:tuple, SthValue:str, _DesignParametertype:int):
+        if _DesignParametertype == 1:
+            ElementType = 'Boundary'
+        elif _DesignParametertype == 2:
+            ElementType = 'Path'
+        if '_DesignParameter' not in self.__dict__:
+            raise Exception("There is no DesignParameter.")
 
+        HierElementList = list(hier_element_tuple)
+        if HierElementList[0] not in self._DesignParameter:
+            raise Exception(f"Invalid Hierarchy element name: {HierElementList[0]}")
+        element = self._DesignParameter[HierElementList.pop(0)]
+
+        for hierarchy_element in HierElementList:
+            if hierarchy_element not in element['_DesignObj']._DesignParameter:
+                raise Exception(f"Invalid Hierarchy element name: {hierarchy_element}.")
+            element = element['_DesignObj']._DesignParameter[hierarchy_element]
+
+        if element['_DesignParametertype'] not in (_DesignParametertype, 7):                # 1: Boundary, 2: Path, 3: Sref
+            raise Exception(f"Only For {ElementType} Element.")
+        return element[SthValue]
+
+
+    def getXWidth(self, *hier_element_tuple:str):
+        """ Get XWidth of hierarchical designObj.
+
+        Note:
+            Input should start from the top hierarchical designObj.\n
+            This function can only be used on 'BoundaryElement'.
+        Args:
+            hier_element_tuple (str): hierarchical designObj Names.
+        Returns:
+            int | float: XWidth Of Lowest Hierarchical designObj.
+        Example:
+            >>> self.getXWidth('_Inverter', '_PMOS', '_POLayer')
+            30
+        """
+        return self._getSthValue(hier_element_tuple=hier_element_tuple, SthValue='_XWidth', _DesignParametertype=1)
+
+    def getYWidth(self, *hier_element_tuple:str):
+        """ Get YWidth of hierarchical designObj.
+
+        Note:
+            Input should start from the top hierarchical designObj.\n
+            This function can only be used on 'BoundaryElement'.
+        Args:
+            hier_element_tuple (str): hierarchical designObj Names.
+        Returns:
+            int | float: YWidth Of Lowest Hierarchical designObj.
+        Example:
+            >>> self.getYWidth('_Inverter', '_PMOS', '_POLayer')
+            1000
+        """
+        return self._getSthValue(hier_element_tuple=hier_element_tuple, SthValue='_YWidth', _DesignParametertype=1)
+
+    def getWidth(self, *hier_element_tuple:str):
+        """ Get Width of hierarchical designObj.
+
+        Note:
+            Input should start from the top hierarchical designObj.\n
+            This function can only be used on 'PathElement'.
+        Args:
+            hier_element_tuple (str): hierarchical designObj Names.
+        Returns:
+            int | float: Width Of Lowest Hierarchical designObj.
+        Example:
+            >>> self.getWidth('_Inverter', '_PMOSSupplyRouting')
+            50
+        """
+        return self._getSthValue(hier_element_tuple=hier_element_tuple, SthValue='_Width', _DesignParametertype=2)
+
+    def getXY(self, *hier_element_tuple:str):
+        """ Calculate Relative Coordinates of hierarchical designObj.
+
+        Note:
+            Input should start from the top hierarchical designObj.\n
+            This function can be used on 'Boundary/Path/Sref Element'.
+            But PathElement CANNOT be used on Multiple hierarchical designObj.
+        Args:
+            hier_element_tuple (str): hierarchical designObj Names.
+        Returns:
+            list[list[int | float]]: Relative Coordinates Of Lowest Hierarchical designObj.
+        Example:
+            >>> self.getXY('_PMOS', '_POLayer')
+            [[-1000,0], [-500,0], [+500,0], [+1000,0]]
+        """
+        if '_DesignParameter' not in self.__dict__:
+            raise Exception("There is no DesignParameter.")
+
+        HierElementList = list(hier_element_tuple)
+        if HierElementList[0] not in self._DesignParameter:
+            raise Exception(f"Invalid Hierarchy element name: {HierElementList[0]}")
+        element = self._DesignParameter[HierElementList.pop(0)]
+        elementXY = element['_XYCoordinates']
+
+        for hierarchy_element in HierElementList:
+            if hierarchy_element not in element['_DesignObj']._DesignParameter:
+                raise Exception(f"Invalid Hierarchy element name: {hierarchy_element}.")
+            element = element['_DesignObj']._DesignParameter[hierarchy_element]
+
+            if element['_DesignParametertype'] == 2:    # 1: Boundary, 2: Path, 3: Sref
+                raise Exception("Not Available on PathElement.")
+
+            tmpAddedXYs = []
+            for XY1 in elementXY:
+                for XY2 in element['_XYCoordinates']:
+                    tmpAddedXYs.append([XY1[0] + XY2[0], XY1[1] + XY2[1]])      # tmpXYs1 + tmpXYs2
+            elementXY = tmpAddedXYs
+
+        return elementXY
 
     def XYCoordinate2MinMaxXY(self, _XYCoordinates):
         x_list = []
