f8fff349d6210a942f6ed3a082c80932e50aa1d7
jicho0927 <jicho0927@postech.ac.kr>
2022-06-15 23:05:30 +0900

    220615

--- PyQTInterface/MainWindow.py
+++ PyQTInterface/MainWindow.py
@@ -49,7 +49,7 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 
-
+#
 from PyQTInterface import userDefineExceptions
 from PyQTInterface import SetupWindow
 from PyQTInterface import template
