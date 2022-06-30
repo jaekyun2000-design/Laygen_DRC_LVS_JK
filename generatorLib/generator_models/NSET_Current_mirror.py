18ac934afa5315af160e3b7122a5dbb76ab35879
jicho0927 <jicho0927@postech.ac.kr>
2022-06-30 16:00:50 +0900

    220630 current mirror 코드 수정.

--- generatorLib/generator_models/NSET_Current_mirror.py
+++ generatorLib/generator_models/NSET_Current_mirror.py
@@ -4,7 +4,7 @@ import copy
 import math
 from generatorLib import DRC
 from generatorLib.generator_models import nmos_stack_current_mirror
-
+from generatorLib.generator_models import ViaMet32Met4
 
 
 class EasyDebugModule(StickDiagram._StickDiagram):
@@ -19,10 +19,11 @@ class EasyDebugModule(StickDiagram._StickDiagram):
                                   nmos_stack_coarse_param={'nmos1_width':2000,'nmos1_length':500,'nmos1_gate':1,'nmos1_dummy':False,'nmos1_xvt':'RVT','nmos1_pccrit':False,'nmos2_width':2000,'nmos2_length':30,'nmos2_gate':1,'nmos2_dummy':False,'nmos2_xvt':'RVT','nmos2_pccrit':False,'guardring_bot':2,'guardring_top':2,'guardring_left':2,'guardring_right':2,'guardring_width':None,'guardring_height':None, 'diode_connect':True},
                                   nmos_stack_fine_param={'nmos1_width':500,'nmos1_length':500,'nmos1_gate':1,'nmos1_dummy':False,'nmos1_xvt':'RVT','nmos1_pccrit':False,'nmos2_width':2000,'nmos2_length':30,'nmos2_gate':1,'nmos2_dummy':False,'nmos2_xvt':'RVT','nmos2_pccrit':False,'guardring_bot':2,'guardring_top':2,'guardring_left':2,'guardring_right':2,'guardring_width':None,'guardring_height':None, 'diode_connect':True},
                                   nmos_stack_mirror_param={'nmos1_width': 2000, 'nmos1_length': 500, 'nmos1_gate': 1, 'nmos1_dummy': False,'nmos1_xvt': 'RVT', 'nmos1_pccrit': False, 'nmos2_width': 2000, 'nmos2_length': 30,'nmos2_gate': 1, 'nmos2_dummy': False, 'nmos2_xvt': 'RVT', 'nmos2_pccrit': False,'guardring_bot': 2, 'guardring_top': 2, 'guardring_left': 2, 'guardring_right': 2,'guardring_width': None, 'guardring_height': None, 'diode_connect': False},\
-                                  guardring_width=None, guardring_height=None, coarse_num=3, fine_num=4, mirror_num=1, Xnum=3, Ynum=4):
+                                  guardring_width=None, guardring_height=None, coarse_num=3, fine_num=4, mirror_num=1, Xnum=3, Ynum=3):
 
         drc = DRC.DRC()
         _Name = self._DesignParameter['_Name']['_Name']
+        MinSnapSpacing=drc._MinSnapSpacing
 
         _OriginXY=[[0,0]]
 
@@ -36,17 +37,17 @@ class EasyDebugModule(StickDiagram._StickDiagram):
         self._DesignParameter['nmos_mirror']['_DesignObj']._CalculateDesignParameter(**dict(**nmos_stack_mirror_param))
 
         if guardring_width==None :
-            max_guardring_width=max(self._DesignParameter['nmos_coarse']['_DesignObj']._DesignParameter['guardring']['_DesignObj']._DesignParameter['right']['_XYCoordinates'][0][0]-self._DesignParameter['nmos_coarse']['_DesignObj']._DesignParameter['guardring']['_DesignObj']._DesignParameter['left']['_XYCoordinates'][0][0],\
+            max_guardring_width=self.CeilMinSnapSpacing(max(self._DesignParameter['nmos_coarse']['_DesignObj']._DesignParameter['guardring']['_DesignObj']._DesignParameter['right']['_XYCoordinates'][0][0]-self._DesignParameter['nmos_coarse']['_DesignObj']._DesignParameter['guardring']['_DesignObj']._DesignParameter['left']['_XYCoordinates'][0][0],\
                                     self._DesignParameter['nmos_fine']['_DesignObj']._DesignParameter['guardring']['_DesignObj']._DesignParameter['right']['_XYCoordinates'][0][0]-self._DesignParameter['nmos_fine']['_DesignObj']._DesignParameter['guardring']['_DesignObj']._DesignParameter['left']['_XYCoordinates'][0][0],\
-                                    self._DesignParameter['nmos_mirror']['_DesignObj']._DesignParameter['guardring']['_DesignObj']._DesignParameter['right']['_XYCoordinates'][0][0]-self._DesignParameter['nmos_mirror']['_DesignObj']._DesignParameter['guardring']['_DesignObj']._DesignParameter['left']['_XYCoordinates'][0][0])
+                                    self._DesignParameter['nmos_mirror']['_DesignObj']._DesignParameter['guardring']['_DesignObj']._DesignParameter['right']['_XYCoordinates'][0][0]-self._DesignParameter['nmos_mirror']['_DesignObj']._DesignParameter['guardring']['_DesignObj']._DesignParameter['left']['_XYCoordinates'][0][0]), 2*MinSnapSpacing)
 
         elif guardring_width!=None :
             max_guardring_width=guardring_width
 
         if guardring_height==None :
-            max_guardring_height=max(self._DesignParameter['nmos_coarse']['_DesignObj']._DesignParameter['guardring']['_DesignObj']._DesignParameter['top']['_XYCoordinates'][0][1]-self._DesignParameter['nmos_coarse']['_DesignObj']._DesignParameter['guardring']['_DesignObj']._DesignParameter['bot']['_XYCoordinates'][0][1],\
+            max_guardring_height=self.CeilMinSnapSpacing(max(self._DesignParameter['nmos_coarse']['_DesignObj']._DesignParameter['guardring']['_DesignObj']._DesignParameter['top']['_XYCoordinates'][0][1]-self._DesignParameter['nmos_coarse']['_DesignObj']._DesignParameter['guardring']['_DesignObj']._DesignParameter['bot']['_XYCoordinates'][0][1],\
                                     self._DesignParameter['nmos_fine']['_DesignObj']._DesignParameter['guardring']['_DesignObj']._DesignParameter['top']['_XYCoordinates'][0][1]-self._DesignParameter['nmos_fine']['_DesignObj']._DesignParameter['guardring']['_DesignObj']._DesignParameter['bot']['_XYCoordinates'][0][1],\
-                                    self._DesignParameter['nmos_mirror']['_DesignObj']._DesignParameter['guardring']['_DesignObj']._DesignParameter['top']['_XYCoordinates'][0][1]-self._DesignParameter['nmos_mirror']['_DesignObj']._DesignParameter['guardring']['_DesignObj']._DesignParameter['bot']['_XYCoordinates'][0][1])
+                                    self._DesignParameter['nmos_mirror']['_DesignObj']._DesignParameter['guardring']['_DesignObj']._DesignParameter['top']['_XYCoordinates'][0][1]-self._DesignParameter['nmos_mirror']['_DesignObj']._DesignParameter['guardring']['_DesignObj']._DesignParameter['bot']['_XYCoordinates'][0][1]), 2*MinSnapSpacing)
 
         elif guardring_height!=None :
             max_guardring_height=guardring_height
@@ -85,7 +86,7 @@ class EasyDebugModule(StickDiagram._StickDiagram):
 
         for j in range(0, Ynum):
             for i in range(0, Xnum):
-                tmp_array.append([_OriginXY[0][0]-(Xnum-1)/2*max_guardring_width+i*max_guardring_width,_OriginXY[0][1]+(Ynum-1)/2*max_guardring_height-j*max_guardring_height])
+                tmp_array.append([self.CeilMinSnapSpacing(_OriginXY[0][0]-(Xnum-1)/2*max_guardring_width+i*max_guardring_width, 2*MinSnapSpacing),self.CeilMinSnapSpacing(_OriginXY[0][1]+(Ynum-1)/2*max_guardring_height-j*max_guardring_height, 2*MinSnapSpacing)])
 
         for i in range(0, coarse_num) :
             tmp_coarse.append([tmp_array[i][0], tmp_array[i][1]-yoffset_coarse])
@@ -100,13 +101,64 @@ class EasyDebugModule(StickDiagram._StickDiagram):
         self._DesignParameter['nmos_fine']['_XYCoordinates']=tmp_fine
         self._DesignParameter['nmos_mirror']['_XYCoordinates']=tmp_mirror
 
-        del tmp_array
         del tmp_coarse
         del tmp_fine
         del tmp_mirror
 
+        self._DesignParameter['m3_connect_y']=self._PathElementDeclaration(_Layer=DesignParameters._LayerMapping['METAL3'][0], _Datatype=DesignParameters._LayerMapping['METAL3'][1], _Width=None)
+        self._DesignParameter['m3_connect_y']['_Width']=2*drc._MetalxMinWidth
+
+        gate_x=self._DesignParameter['nmos_coarse']['_DesignObj']._DesignParameter['via_m1_m3_nmos1_gate']['_XYCoordinates'][0][0]
+        gate_y=self._DesignParameter['nmos_coarse']['_DesignObj']._DesignParameter['via_m1_m3_nmos1_gate']['_XYCoordinates'][0][1]-yoffset_coarse
+
+        total_num=coarse_num+fine_num+mirror_num
+        tmp_x=Xnum-(Xnum*Ynum - total_num)
+
+        tmp1=[]
+        for i in range(0,tmp_x):
+            tmp1.append([[tmp_array[i][0]+gate_x, tmp_array[0][1]+gate_y], [tmp_array[i][0]+gate_x, tmp_array[-1][1]+gate_y]])
+
+        tmp2=[]
+        for i in range(tmp_x,Xnum):
+            tmp2.append([[tmp_array[i][0]+gate_x, tmp_array[0][1]+gate_y], [tmp_array[i][0]+gate_x, tmp_array[total_num-Xnum][1]+gate_y]])
+
+        self._DesignParameter['m3_connect_y']['_XYCoordinates']=tmp1+tmp2
+        del tmp1
+        del tmp2
+
+        self._DesignParameter['Via_m3_m4']=self._SrefElementDeclaration(_DesignObj=ViaMet32Met4._ViaMet32Met4(_Name='ViaMet32Met4In{}'.format(_Name)))[0]
+        self._DesignParameter['Via_m3_m4']['_DesignObj']._CalculateDesignParameterSameEnclosure(**dict(_ViaMet32Met4NumberOfCOX=2, _ViaMet32Met4NumberOfCOY=2))
+        tmp=[]
+
+        for i in range(Xnum, total_num):
+            tmp.append([tmp_array[i][0]+gate_x, tmp_array[i][1]+self._DesignParameter['nmos_coarse']['_DesignObj']._DesignParameter['guardring']['_DesignObj']._DesignParameter['top']['_XYCoordinates'][0][1]])
+
+        self._DesignParameter['Via_m3_m4']['_XYCoordinates']=tmp
+        del tmp
+
+        tmp=[]
+
+        self._DesignParameter['m4_connect_x']=self._PathElementDeclaration(_Layer=DesignParameters._LayerMapping['METAL4'][0], _Datatype=DesignParameters._LayerMapping['METAL4'][1], _Width=None)
+        self._DesignParameter['m4_connect_x']['_Width']=2*drc._MetalxMinWidth
+
+        for j in range(0, Ynum-2):
+            tmp.append([[tmp_array[0][0]+gate_x, tmp_array[Xnum*j][1]+self._DesignParameter['nmos_coarse']['_DesignObj']._DesignParameter['guardring']['_DesignObj']._DesignParameter['bot']['_XYCoordinates'][0][1]], [tmp_array[Xnum-1][0]+gate_x, tmp_array[Xnum*j][1]+self._DesignParameter['nmos_coarse']['_DesignObj']._DesignParameter['guardring']['_DesignObj']._DesignParameter['bot']['_XYCoordinates'][0][1]]])
+
+        tmp.append([[tmp_array[0][0]+gate_x, self._DesignParameter['Via_m3_m4']['_XYCoordinates'][-1][1]], [tmp_array[total_num-1][0]+gate_x, self._DesignParameter['Via_m3_m4']['_XYCoordinates'][-1][1]]])
+
+        self._DesignParameter['m4_connect_x']['_XYCoordinates']=tmp
+        del tmp
+
         if coarse_num+fine_num+mirror_num > Xnum*Ynum :
             raise NotImplementedError
 
+        if (nmos_stack_coarse_param['nmos1_length'] == nmos_stack_fine_param['nmos1_length'] == nmos_stack_mirror_param['nmos1_length']) == False :
+            raise NotImplementedError
+
+        if (nmos_stack_coarse_param['nmos2_length'] == nmos_stack_fine_param['nmos2_length'] == nmos_stack_mirror_param['nmos2_length']) == False :
+            raise NotImplementedError
+
+        if Xnum*Ynum % total_num >= Xnum :
+            raise NotImplementedError
 
 
