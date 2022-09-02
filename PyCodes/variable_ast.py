66b3fa025b8897f645ed2f5087755e772aca4f05
sun9u <sgjeong@postech.ac.kr>
2022-09-02 10:45:20 +0900

    cont x, y 개수 자동으로 계산해주는 함수 추가

--- PyCodes/variable_ast.py
+++ PyCodes/variable_ast.py
@@ -1618,6 +1618,21 @@ class CustomFunctionTransformer(ast.NodeTransformer):
 
         return f"{base_element_string}['_Reflect'] = {reflection}"
 
+    def transform_cont(self, node):
+        arg_names, arg_indexes = self.parse_args_info(node.args)
+        arg_names.append('_POLayer')
+        poly_last_arg_indexes = ['[0]', '[-1]']
+        poly_first_arg_indexes = ['[0]', '[0]']
+        base_element_string = self.translate_base_string(arg_names)
+        base_xy_right_string_tuple = self.extract_xy_hierarchy_string(arg_names, poly_last_arg_indexes, True)
+        base_xy_left_string_tuple = self.extract_xy_hierarchy_string(arg_names, poly_first_arg_indexes, True)
+
+        output_x = f"{base_xy_right_string_tuple[0]} - {base_xy_left_string_tuple[0]} + {base_element_string}['_XWidth']"
+        output_x = f"max(1, 1+round( ({output_x} -drc._CoMinWidth - 2 * drc._CoMinEnclosureByPOAtLeastTwoSide )/  ( drc._CoMinWidth + drc._CoMinSpace ) ) )"
+        output_y = f"2 if {output_x} == 1 else 1"
+        return f"dict(_ViaPoly2Met1NumberOfCOX = {output_x}, _ViaPoly2Met1NumberOfCOY = {output_y})"
+        # return f"max(1, 1+int( ({target_width} -drc._CoMinSpace - 2 * drc._CoMinEnclosureByPO )/  ( drc._CoMinWidth + drc._CoMinSpace ) ) )"
+
     # def transform_len(self, node):
     #     arg_names, arg_indexes = self.parse_args_info(node.args)
     #     # base_element_string = self.translate_base_string(arg_names)
