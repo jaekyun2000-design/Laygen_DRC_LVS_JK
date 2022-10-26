375be214bb376be4a719e1246966c66f99511a0f
sun9u <sgjeong@postech.ac.kr>
2022-10-26 15:14:51 +0900

    element prediction에 필요한 정보를 user_setup에서 --> element_predictor로 변경 generator model loading실패 해도 작동하도록

--- powertool/model/element_predictor.py
+++ powertool/model/element_predictor.py
@@ -11,7 +11,18 @@ if user_setup.DL_FEATURE or user_setup.DDL_FEATURE:
 
 
 if user_setup.DL_FEATURE:
-    def create_element_detector_model():
+    model_dir = user_setup.model_dir
+    with open(f'{model_dir}/matrix_size.bin', 'rb') as f:
+        matrix_size = pickle.load(f)
+    with open(f'{model_dir}/layer_list.bin', 'rb') as f:
+        layer_list = pickle.load(f)
+    with open(f'{model_dir}/class_list.bin', 'rb') as f:
+        data_type_list = pickle.load(f)
+    matrix_x_step = matrix_size[0]
+    matrix_y_step = matrix_size[1]
+
+
+    def create_element_detector_model_old():
         if user_setup.matrix_x_step == 128:
             if user_setup._Technology == 'TSMC65nm':
                 # return tensorflow.keras.models.load_model('powertool/dl_models/tsmc65/cell_detection')
@@ -33,6 +44,30 @@ if user_setup.DL_FEATURE:
             # elif user_setup._Technology == 'SS28nm':
             #     return tensorflow.keras.models.load_model('powertool/dl_models/tsmc65/small_dataset_model_keras')
                 # return tensorflow.keras.models.load_model('powertool/dl_models/ss28/100_dataset_model_size2_keras')
+
+    def create_element_detector_model(model_dir='./powertool/dl_models/ss28/128b_class3a'):
+        with open(f'{model_dir}/class_list.bin', 'rb') as f:
+            class_list = pickle.load(f)
+        with open(f'{model_dir}/matrix_size.bin', 'rb') as f:
+            matrix_size = pickle.load(f)
+        with open(f'{model_dir}/layer_list.bin', 'rb') as f:
+            layer_list = pickle.load(f)
+        return create_model(class_list, matrix_size, layer_list, model_dir)
+
+    def create_model(class_list, matrix_size, layer_list, model_dir):
+        model = tf.keras.models.Sequential()
+        model.add(tf.keras.layers.Conv2D(8, (1, 1), activation='relu', input_shape=(matrix_size[0], matrix_size[1], len(layer_list))))
+        model.add(tf.keras.layers.Conv2D(16, (3, 3), activation='relu'))
+        model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+        model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu'))
+        model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+        model.add(tf.keras.layers.Conv2D(16, (1, 1), activation='relu'))
+        model.add(tf.keras.layers.Dropout(0.4))
+        model.add(tf.keras.layers.Flatten())
+        model.add(tf.keras.layers.Dense(len(class_list), activation='sigmoid'))
+        latest_checkpoint = tf.train.latest_checkpoint(model_dir)
+        model.load_weights(latest_checkpoint)
+        return model
 
 
     model = None
