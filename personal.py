9cd702445b63a74d16fc554fa0ffe85a5a16b281
jihoon0721 <jack990721@gmail.com>
2024-03-13 22:21:51 +0900

    personal.py file

---
+++ personal.py
@@ -0,0 +1,5 @@
+hostname = '141.223.24.53' # 서버 주소
+username = 'jihoon0721' # 서버 계정명
+password = 'password' # 서버 로그인 비밀번호
+TECHDIR = '/mnt/sdb/jihoon0721/OPUS/ss28nm' # setup 파일이 있는 주소
+RUNDIR = '/mnt/sdb/jihoon0721/OPUS/ss28nm/RandGen' # DRC, LVS, PEX, posim 실행 주소
