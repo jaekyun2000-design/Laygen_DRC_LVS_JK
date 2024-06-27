052e3bca661031257a20e827f910ad1549d5073e
ljw95 <jwlee95@postech.ac.kr>
2024-06-27 14:52:22 +0900

    Deserializer용 Block

--- generatorLib/generator_models/Deserializer_testbench.py
+++ generatorLib/generator_models/Deserializer_testbench.py
@@ -2,7 +2,8 @@ from generatorLib import StickDiagram
 from generatorLib import DesignParameters
 from generatorLib import DRC
 from generatorLib.generator_models import Deserializer1toN
-
+import time
+start = time.time()
 class Deserializer1toN_tb(StickDiagram._StickDiagram):
 
     def __init__(self, _DesignParameter=None, _Name='Deserializer1toN'):
@@ -207,42 +208,59 @@ class Deserializer1toN_tb(StickDiagram._StickDiagram):
 ################################ DRC Check #################################
 import random
 if __name__ == '__main__':
-        list = [2, 4, 8, 16, 32]
-        Deserialize1toN = random.choice(list)
-        TG1_Finger = random.randint(1, 10)
-        TG2_Finger = random.randint(1, 10)
+        # list = [2, 4, 8, 16, 32]
+        # Deserialize1toN = random.choice(list)
+        # TG1_Finger = random.randint(1, 10)
+        # TG2_Finger = random.randint(1, 10)
+        #
+        # INV1_Finger = random.randint(1, 10)
+        # INV2_Finger = random.randint(1, 10)
+        # INV3_Finger = random.randint(1, 10)
+        # INV4_Finger = random.randint(1, 10)
+        #
+        # TG3_Finger = random.randint(1, 10)
+        # INV5_Finger = random.randint(1, 10)
+        # INV6_Finger = random.randint(1, 10)
+        #
+        # TG4_Finger = random.randint(1, 10)
+        #
+        # INV7_Finger = random.randint(1, 10)
+        # INV8_Finger = random.randint(1, 10)
+        # INV9_Finger = random.randint(1, 10)
+        # INV10_Finger = random.randint(1, 10)
 
-        INV1_Finger = random.randint(1, 10)
-        INV2_Finger = random.randint(1, 10)
-        INV3_Finger = random.randint(1, 10)
-        INV4_Finger = random.randint(1, 10)
+        TG1_Finger = 1
+        TG2_Finger = 2
+        TSI1_Finger = 1
+        TSI2_Finger = 1
+        INV1_Finger = 3
+        INV2_Finger = 1
+        INV3_Finger = 1
+        INV4_Finger = 3
 
-        TG3_Finger = random.randint(1, 10)
-        INV5_Finger = random.randint(1, 10)
-        INV6_Finger = random.randint(1, 10)
-
-        TG4_Finger = random.randint(1, 10)
-
-        INV7_Finger = random.randint(1, 10)
-        INV8_Finger = random.randint(1, 10)
-        INV9_Finger = random.randint(1, 10)
-        INV10_Finger = random.randint(1, 10)
-
-
+        TG3_Finger = 2
+        TSI3_Finger = 1
+        INV5_Finger = 4
+        INV6_Finger = 4
+        TG4_Finger = 2
+        TSI4_Finger = 1
+        INV7_Finger = 4
+        INV8_Finger = 4
+        INV9_Finger = 1
+        INV10_Finger = 1
 
         Deserialize1toN = 32
         # TSI1_Finger=TSI2_Finger=TSI3_Finger=TSI4_Finger=1
         # random.randrange(200, 250, 10)
 
-        np_ratio = random.randint(2, 3)
+        np_ratio = 2#random.randint(2, 3)
         TG1_NMWidth = TG2_NMWidth = TG3_NMWidth = TG4_NMWidth = TSI1_NMWidth = TSI2_NMWidth = TSI3_NMWidth = TSI4_NMWidth = INV1_NMWidth = INV2_NMWidth = INV3_NMWidth \
-            = INV4_NMWidth = INV5_NMWidth = INV6_NMWidth = INV7_NMWidth = INV8_NMWidth = INV9_NMWidth = INV10_NMWidth = random.randrange(
-            200, 400, 50)  # default 200~400
+            = INV4_NMWidth = INV5_NMWidth = INV6_NMWidth = INV7_NMWidth = INV8_NMWidth = INV9_NMWidth = INV10_NMWidth = 200#random.randrange(200, 400, 50)  # default 200~400
         TG1_PMWidth = TG2_PMWidth = TG3_PMWidth = TG4_PMWidth = TSI1_PMWidth = TSI2_PMWidth = TSI3_PMWidth = TSI4_PMWidth = INV1_PMWidth = INV2_PMWidth = INV3_PMWidth \
             = INV4_PMWidth = INV5_PMWidth = INV6_PMWidth = INV7_PMWidth = INV8_PMWidth = INV9_PMWidth = INV10_PMWidth = TG1_NMWidth * np_ratio  # random.randint(2, 3)  ### np ratio = 2~3사이 가능
 
-        dummy = False  # random.randint(0, 1) #only use 1:N archetecture
-        ChannelLength = random.choice([30, 40])
+        dummy = False  # random.randint(0, 1) #only use 1:32 archetecture
+        ChannelLength = 30# random.choice([30, 40])
         GateSpacing = 100
         SDWidth = 66
         list_XVT = ['SLVT', 'RVT', 'LVT', 'HVT']
@@ -251,26 +269,7 @@ if __name__ == '__main__':
         CellHeight = 1800  # 3000
         SupplyRailType = 2  # random.randint(1, 2)
 
-        # TG1_Finger = 1
-        # TG2_Finger = 2
-        # TSI1_Finger = 1
-        # TSI2_Finger = 1
-        # INV1_Finger = 3
-        # INV2_Finger = 1
-        # INV3_Finger = 1
-        # INV4_Finger = 3
-        #
-        # TG3_Finger = 2
-        # TSI3_Finger = 1
-        # INV5_Finger = 4
-        # INV6_Finger = 4
-        # TG4_Finger = 2
-        # TSI4_Finger = 1
-        # INV7_Finger = 4
-        # INV8_Finger = 4
-        # INV9_Finger = 1
-        # INV10_Finger = 1
-        #
+
         #
         # Deserialize1toN=32
         #
@@ -399,7 +398,7 @@ if __name__ == '__main__':
         myfile.close()
         import DRCchecker
 
-
+        end = time.time()
         print("TG1_Finger = ", TG1_Finger)
         print("TG2_Finger = ", TG2_Finger)
         print("TG3_Finger = ", TG3_Finger)
@@ -426,3 +425,6 @@ if __name__ == '__main__':
         print('      Finished       '.center(105, '#'))
         # print("DRC Clean!!!")
 
+        print(f"{end - start:.5f} sec")
+        # generation_time = time.time()
+        # print("Generation time=",generation_time)
